6eeb2f22c817fc5bb16db5922ff4f59378130973
María Inés Parnisari <maineparnisari@gmail.com>
2014-10-27 21:04:57 -0300

    Corrijo errores de compilacion

--- tp2/src/ConsoleApplication/Main.cs
+++ tp2/src/ConsoleApplication/Main.cs
@@ -1,9 +1,5 @@
 using System.Configuration;
 using System.Diagnostics;
-using TP1.Influences;
-using TP1.Sort;
-using TP1.Recommendations;
-using TP1.GraphReader;
 
 namespace ConsoleApplication
 {
@@ -16,30 +12,7 @@ namespace ConsoleApplication
             var logger = new Logger(ConfigurationManager.AppSettings["logFileName"]);
             try
             {
-                var filePath = ConfigurationManager.AppSettings["inputFilePath"];
-                var builder = new GraphBuilder(new GraphReader(filePath));
-                var graph = builder.Build();
-
-                // Punto 1
-                logger.Log("POPULARIDAD:");
-                foreach (var node in graph.Sort((nodeA, nodeB) => nodeA.Degree >= nodeB.Degree))
-                {
-                    logger.Log(node.Data.Name + " [" + node.Degree + "]");
-                }
-
-                // Punto 2
-                logger.Log("INFLUENCIAS:");
-                foreach (var influence in graph.GetInfluences().OrderByDescending())
-                {
-                    logger.Log(influence.Node.Data.Name + " [" + influence.Value + "]");
-                }
-
-                // Punto 3
-                logger.Log("RECOMENDACIONES DE AMIGOS:");
-                foreach (var r in graph.GetRecommendations().Recommendations)
-                {
-                    logger.Log(r.Person.Data.Name + " => " + r.PersonToRecommend.Data.Name + " [" + r.FriendCount + "]");
-                }
+                // Code goes here
             }
             catch (System.Exception)
             {
